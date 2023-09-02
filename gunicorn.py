580559e07e8a9a14d407ee895b1e7ac17d995c4c
hazemAzzam <61450444+hazemAzzam@users.noreply.github.com>
2023-09-02 17:57:21 +0300

    fix

--- gunicorn.py
+++ gunicorn.py
@@ -1,6 +1,6 @@
-# gunicorn_config.py
+# gunicorn.py
 
-bind = '0.0.0.0:8000'  # Replace with your desired host and port
-workers = 4  # Adjust the number of workers as needed
-module = 'university.asgi:application'  # Replace 'your_project_name' with your actual project name
-
+bind = '0.0.0.0:8000'
+workers = 4
+pythonpath = '/www/wwwroot/University'
+module = 'university.asgi:application'
